732439b8eaf7f802299e221924a2424bce5cc1bc
armaniferrante <armaniferrante@gmail.com>
2020-09-23 23:40:32 -0700

    Safe integration tests

---
+++ safe/tests/mint-locked.rs
@@ -0,0 +1,142 @@
+use common::assert::assert_eq_vec;
+use serum_common::pack::Pack;
+use serum_safe::accounts::{MintReceipt, Vesting};
+use solana_client_gen::solana_sdk::commitment_config::CommitmentConfig;
+use solana_client_gen::solana_sdk::instruction::AccountMeta;
+use solana_client_gen::solana_sdk::signature::Signer;
+use solana_client_gen::solana_sdk::sysvar;
+use spl_token::option::COption;
+use spl_token::pack::Pack as TokenUnpack;
+use spl_token::state::Mint;
+
+mod common;
+
+#[test]
+fn mint() {
+    // Given.
+    //
+    // An initialized Serum Safe with deposit.
+    let common::lifecycle::Deposited {
+        client,
+        vesting_acc,
+        vesting_acc_beneficiary,
+        vesting_acc_slots,
+        vesting_acc_amounts,
+        safe_acc,
+        safe_srm_vault_authority,
+        ..
+    } = common::lifecycle::deposit();
+
+    // When.
+    //
+    // I mint locked srm.
+    let nft_token_acc_owner = vesting_acc_beneficiary.pubkey();
+    let nft_count = 2;
+    let lsrm_nfts = {
+        let mint_lsrm_accs = vec![
+            AccountMeta::new(vesting_acc_beneficiary.pubkey(), true),
+            AccountMeta::new(vesting_acc, false),
+            AccountMeta::new_readonly(safe_acc, false),
+            AccountMeta::new(safe_srm_vault_authority, false),
+            AccountMeta::new(spl_token::ID, false),
+            AccountMeta::new_readonly(sysvar::rent::ID, false),
+        ];
+        let signers = vec![&vesting_acc_beneficiary, client.payer()];
+        let (_sig, lsrm_nfts) = client
+            .create_nfts_and_mint_locked_with_signers(
+                nft_count,
+                &nft_token_acc_owner,
+                signers,
+                mint_lsrm_accs,
+            )
+            .unwrap();
+        lsrm_nfts
+    };
+
+    // Then.
+    //
+    // The lsrm nft mints should be initialized.
+    {
+        let lsrm_nft_mints = lsrm_nfts.iter().map(|lsrm| {
+            let account = client
+                .rpc()
+                .get_account_with_commitment(&lsrm.mint.pubkey(), CommitmentConfig::recent())
+                .unwrap()
+                .value
+                .unwrap();
+            Mint::unpack_unchecked(&account.data).unwrap()
+        });
+
+        for mint in lsrm_nft_mints {
+            assert!(mint.is_initialized);
+            assert_eq!(mint.mint_authority, COption::None);
+            assert_eq!(mint.supply, 1);
+            assert_eq!(mint.decimals, 0);
+            assert_eq!(mint.freeze_authority, COption::None);
+        }
+    }
+    // Then.
+    //
+    // The lsrm nft token accounts should be initialized.
+    {
+        let token_accs = lsrm_nfts.iter().map(|lsrm| {
+            let account = serum_common::client::rpc::account_token_unpacked::<
+                spl_token::state::Account,
+            >(client.rpc(), &lsrm.token_acc.pubkey());
+            (lsrm, account)
+        });
+        for (lsrm, ta) in token_accs {
+            assert_eq!(ta.state, spl_token::state::AccountState::Initialized);
+            assert_eq!(ta.owner, nft_token_acc_owner);
+            assert_eq!(ta.mint, lsrm.mint.pubkey());
+            assert_eq!(ta.amount, 1);
+        }
+    }
+    // Then.
+    //
+    // The lsrm receipts should be initialized.
+    {
+        let lsrm_receipts = lsrm_nfts.iter().map(|lsrm| {
+            let account = client
+                .rpc()
+                .get_account_with_commitment(&lsrm.receipt, CommitmentConfig::recent())
+                .unwrap()
+                .value
+                .unwrap();
+            (MintReceipt::unpack(&account.data).unwrap(), lsrm)
+        });
+        for (idx, (receipt, lsrm)) in lsrm_receipts.enumerate() {
+            assert!(receipt.initialized);
+            assert_eq!(receipt.mint, lsrm_nfts[idx].mint.pubkey());
+            assert_eq!(receipt.vesting_acc, vesting_acc);
+            assert_eq!(receipt.burned, false);
+            assert_eq!(receipt.token_acc, lsrm.token_acc.pubkey());
+        }
+    }
+    // Then.
+    //
+    // The vesting accounts should be updated.
+    {
+        let updated_vesting_acc = {
+            let account = client
+                .rpc()
+                .get_account_with_commitment(&vesting_acc, CommitmentConfig::recent())
+                .unwrap()
+                .value
+                .unwrap();
+            Vesting::unpack(&account.data).unwrap()
+        };
+
+        // The field we care about.
+        assert_eq!(updated_vesting_acc.locked_outstanding, nft_count as u64);
+        // Sanity check the rest.
+        assert_eq!(updated_vesting_acc.safe, safe_acc);
+        assert_eq!(
+            updated_vesting_acc.beneficiary,
+            vesting_acc_beneficiary.pubkey()
+        );
+        assert_eq!(updated_vesting_acc.initialized, true);
+        assert_eq_vec(updated_vesting_acc.slots, vesting_acc_slots);
+        assert_eq_vec(updated_vesting_acc.amounts, vesting_acc_amounts);
+    }
+}
---
+++ safe/tests/withdraw.rs
@@ -0,0 +1,285 @@
+use common::assert::assert_eq_vec;
+use common::lifecycle;
+use rand::rngs::OsRng;
+use serum_common::pack::Pack;
+use serum_safe::accounts::Vesting;
+use serum_safe::error::SafeErrorCode;
+use solana_client_gen::solana_sdk::commitment_config::CommitmentConfig;
+use solana_client_gen::solana_sdk::instruction::AccountMeta;
+use solana_client_gen::solana_sdk::signature::Keypair;
+use solana_client_gen::solana_sdk::signature::Signer;
+use solana_client_gen::solana_sdk::sysvar;
+use spl_token::pack::Pack as TokenPack;
+
+mod common;
+
+// Summary.
+//
+// * Vesting amount of 30 for first period.
+// * First vesting period passes.
+// * Withdraw 30 SRM.
+//
+// Should receive the SRM.
+#[test]
+fn withdraw() {
+    withdraw_test(WithdrawTestParams {
+        test_type: TestType::Normal,
+        vesting_slot_offsets: vec![1, 100_000, 200_000],
+        vesting_amounts: vec![30, 40, 50],
+        expected_vesting_amounts: vec![0, 40, 50],
+        expected_withdraw_amount: 30,
+        expected_spl_balance: 30,
+        slot_wait_index: Some(0),
+        error_code: None,
+    })
+}
+
+// Summary.
+//
+// * Vesting amount of 30.
+// * Vesting period passes.
+// * Withdraw 31.
+//
+// Should not receive anything.
+#[test]
+fn withdraw_more_than_vested() {
+    withdraw_test(WithdrawTestParams {
+        test_type: TestType::Normal,
+        vesting_slot_offsets: vec![1, 100_000, 200_000],
+        vesting_amounts: vec![30, 40, 50],
+        expected_vesting_amounts: vec![30, 40, 50],
+        expected_withdraw_amount: 31,
+        expected_spl_balance: 0,
+        slot_wait_index: Some(0),
+        error_code: Some(SafeErrorCode::InsufficientBalance.into()),
+    })
+}
+
+// Summary.
+//
+// * Vesting amount of 30.
+// * Vesting period does not pass.
+// * Withdraw 1.
+//
+// Should not receive anything.
+#[test]
+fn withdraw_without_vesting() {
+    withdraw_test(WithdrawTestParams {
+        test_type: TestType::Normal,
+        vesting_slot_offsets: vec![99_000, 100_000, 200_000],
+        vesting_amounts: vec![30, 40, 50],
+        expected_vesting_amounts: vec![30, 40, 50],
+        expected_withdraw_amount: 100,
+        expected_spl_balance: 0,
+        slot_wait_index: None,
+        error_code: Some(SafeErrorCode::InsufficientBalance.into()),
+    })
+}
+
+// Summary.
+//
+// * Vesting amount of 30.
+// * Mint 2 lSRM.
+// * Vesting period passes.
+// * Withdraw 20 SRM.
+//
+// Should receive the SRM.
+#[test]
+fn withdraw_some_when_locked_outstanding() {
+    withdraw_test(WithdrawTestParams {
+        test_type: TestType::LsrmMinted(2),
+        vesting_slot_offsets: vec![1, 100_000, 200_000],
+        vesting_amounts: vec![30, 40, 50],
+        expected_vesting_amounts: vec![10, 40, 50],
+        expected_withdraw_amount: 20,
+        expected_spl_balance: 20,
+        slot_wait_index: Some(0),
+        error_code: None,
+    })
+}
+
+// Summary.
+//
+// * Vesting amount of 30.
+// * Mint 2 LSRM.
+// * Vesting period passes.
+// * Withdraw 30 SRM.
+//
+// Should not receive anything.
+#[test]
+fn withdraw_all_when_locked_outstanding() {
+    withdraw_test(WithdrawTestParams {
+        test_type: TestType::LsrmMinted(2),
+        vesting_slot_offsets: vec![1, 100_000, 200_000],
+        vesting_amounts: vec![30, 40, 50],
+        expected_vesting_amounts: vec![30, 40, 50],
+        expected_withdraw_amount: 30,
+        expected_spl_balance: 0,
+        slot_wait_index: Some(0),
+        error_code: Some(SafeErrorCode::InsufficientBalance.into()),
+    })
+}
+
+fn withdraw_test(params: WithdrawTestParams) {
+    let WithdrawTestParams {
+        test_type,
+        vesting_slot_offsets,
+        vesting_amounts,
+        expected_vesting_amounts,
+        expected_withdraw_amount,
+        expected_spl_balance,
+        slot_wait_index,
+        error_code,
+    } = params;
+    // Given.
+    //
+    // A vesting account.
+    let StartState {
+        client,
+        vesting_acc,
+        vesting_acc_beneficiary,
+        vesting_acc_slots,
+        safe_acc,
+        safe_srm_vault,
+        safe_srm_vault_authority,
+        srm_mint,
+        ..
+    } = start_state(test_type, vesting_slot_offsets, vesting_amounts);
+    // And.
+    //
+    // An empty SRM SPL token account.
+    let beneficiary_srm_spl_acc = serum_common::client::rpc::create_token_account(
+        client.rpc(),
+        &srm_mint.pubkey(),
+        &vesting_acc_beneficiary.pubkey(),
+        client.payer(),
+    )
+    .unwrap();
+
+    // When.
+    //
+    // The vesting period passes (or doesn't if set to None).
+    if let Some(slot_wait_index) = slot_wait_index {
+        common::blockchain::pass_time(client.rpc(), vesting_acc_slots[slot_wait_index]);
+    }
+    // And.
+    //
+    // I withdraw from the vesting account *to* the empty SPL token account.
+    {
+        let accounts = [
+            AccountMeta::new_readonly(vesting_acc_beneficiary.pubkey(), true),
+            AccountMeta::new(vesting_acc, false),
+            AccountMeta::new(beneficiary_srm_spl_acc.pubkey(), false),
+            AccountMeta::new(safe_srm_vault.pubkey(), false),
+            AccountMeta::new_readonly(safe_srm_vault_authority, false),
+            AccountMeta::new(safe_acc, false),
+            AccountMeta::new_readonly(spl_token::ID, false),
+            AccountMeta::new_readonly(sysvar::clock::ID, false),
+        ];
+        let signers = [&vesting_acc_beneficiary, client.payer()];
+        let r = client.withdraw_with_signers(&signers, &accounts, expected_withdraw_amount);
+        if error_code.is_some() {
+            match r {
+                Ok(_) => panic!("expected error code from withdrawal"),
+                Err(client_error) => {
+                    assert_eq!(client_error.error_code(), error_code);
+                }
+            };
+        }
+    };
+    // Then.
+    //
+    // I should have SRM in my account.
+    {
+        let spl_acc = {
+            let account = client
+                .rpc()
+                .get_account_with_commitment(
+                    &beneficiary_srm_spl_acc.pubkey(),
+                    CommitmentConfig::recent(),
+                )
+                .unwrap()
+                .value
+                .unwrap();
+            spl_token::state::Account::unpack_from_slice(&account.data).unwrap()
+        };
+        assert_eq!(spl_acc.amount, expected_spl_balance);
+    }
+
+    // Then.
+    //
+    // My vesting account amounts should be updated.
+    {
+        let vesting_acc = {
+            let account = client
+                .rpc()
+                .get_account_with_commitment(&vesting_acc, CommitmentConfig::recent())
+                .unwrap()
+                .value
+                .unwrap();
+            Vesting::unpack(&account.data).unwrap()
+        };
+        assert_eq_vec(vesting_acc.amounts, expected_vesting_amounts);
+    }
+}
+
+// Alisasing LsrmMinted type here because we don't use the `lsrm` field in these
+// tests and so we can avoid making another needless type.
+type StartState = lifecycle::LsrmMinted;
+
+fn start_state(
+    test_type: TestType,
+    vesting_slot_offsets: Vec<u64>,
+    vesting_amounts: Vec<u64>,
+) -> StartState {
+    match test_type {
+        TestType::LsrmMinted(lsrm_count) => {
+            lifecycle::mint_lsrm(lsrm_count, vesting_slot_offsets, vesting_amounts)
+        }
+        TestType::Normal => {
+            let lifecycle::Deposited {
+                client,
+                vesting_acc,
+                vesting_acc_beneficiary,
+                vesting_acc_slots,
+                vesting_acc_amounts,
+                safe_acc,
+                safe_srm_vault,
+                safe_srm_vault_authority,
+                srm_mint,
+                ..
+            } = lifecycle::deposit_with_schedule(vesting_slot_offsets, vesting_amounts);
+            // Dummy keypair to stuff into type. Not used.
+            let lsrm_token_acc_owner = Keypair::generate(&mut OsRng);
+            lifecycle::LsrmMinted {
+                client,
+                vesting_acc,
+                vesting_acc_beneficiary,
+                vesting_acc_slots,
+                vesting_acc_amounts,
+                safe_acc,
+                safe_srm_vault,
+                safe_srm_vault_authority,
+                srm_mint,
+                lsrm: vec![],
+                lsrm_token_acc_owner,
+            }
+        }
+    }
+}
+
+struct WithdrawTestParams {
+    test_type: TestType,
+    vesting_slot_offsets: Vec<u64>,
+    vesting_amounts: Vec<u64>,
+    expected_vesting_amounts: Vec<u64>,
+    expected_withdraw_amount: u64,
+    expected_spl_balance: u64,
+    slot_wait_index: Option<usize>,
+    error_code: Option<u32>,
+}
+
+enum TestType {
+    Normal,
+    LsrmMinted(usize),
+}
