cfcff052357339abf1a47282229b700cc132d65f
armaniferrante <armaniferrante@gmail.com>
2020-10-01 23:44:03 -0700

    Remove travis caching

--- scripts/travis/run-docker.sh
+++ scripts/travis/run-docker.sh
@@ -6,23 +6,8 @@ main() {
     docker pull projectserum/development:latest
     #
     # Bind the relevant host directories to the docker image so that the
-    # relevant files are synced.
+    # files are synced.
     #
-    docker volume create --driver local \
-           --opt type=none \
-           --opt device=$TRAVIS_HOME/.cargo \
-           --opt o=bind \
-           cargodir
-    docker volume create --driver local \
-           --opt type=none \
-           --opt device=$TRAVIS_HOME/.rustup \
-           --opt o=bind \
-           rustupdir
-    docker volume create --driver local \
-           --opt type=none \
-           --opt device=$TRAVIS_HOME/.cache \
-           --opt o=bind \
-           cachedir
     docker volume create --driver local \
            --opt type=none \
            --opt device=$TRAVIS_BUILD_DIR \
@@ -32,9 +17,6 @@ main() {
     # Start the container.
     #
     docker run -it -d --net host --name dev \
-           -v cargodir:/root/.cargo \
-           -v rustupdir:/root/.rustup \
-           -v cachedir:/root/.cache \
            -v workdir:/workdir \
            projectserum/development:latest bash
 }
