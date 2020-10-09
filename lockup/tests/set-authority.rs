402db4b10d1f2ffbb4b02b68a0a057140659c22f
armaniferrante <armaniferrante@gmail.com>
2020-10-08 17:38:24 -0700

    Rename safe and new client

---
+++ lockup/tests/set-authority.rs
@@ -0,0 +1,75 @@
+use common::lifecycle::{self, Initialized};
+use solana_client_gen::prelude::*;
+use serum_lockup_client::*;
+use serum_lockup::accounts::Safe;
+use solana_client_gen::solana_sdk::instruction::AccountMeta;
+use solana_client_gen::solana_sdk::pubkey::Pubkey;
+use solana_client_gen::solana_sdk::signature::Signer;
+
+mod common;
+
+#[test]
+fn set_authority() {
+    // Given.
+    //
+    // An initialized safe.
+    let Initialized {
+        client,
+        safe_acc,
+        safe_authority,
+        ..
+    } = lifecycle::initialize();
+
+    // When.
+    //
+    // I set the authority to someone new.
+    let new_authority = Pubkey::new_rand();
+		let _ = client.set_authority(SetAuthorityRequest {
+				authority: &safe_authority,
+				safe: safe_acc,
+				new_authority,
+		}).unwrap();
+
+    // Then.
+    //
+    // The safe account should be updated.
+    {
+        let safe_acc: Safe = client.safe(&safe_acc).unwrap();
+        assert_eq!(safe_acc.authority, new_authority);
+    }
+}
+
+#[test]
+fn set_authority_zero() {
+    // Given.
+    //
+    // An initialized safe.
+    let Initialized {
+        client,
+        safe_acc,
+        safe_authority,
+        ..
+    } = lifecycle::initialize();
+
+    // When.
+    //
+    // I set the authority to zero.
+    let new_authority = Pubkey::new_from_array([0; 32]);
+    let accounts = [
+        AccountMeta::new_readonly(safe_authority.pubkey(), true),
+        AccountMeta::new(safe_acc, false),
+    ];
+    let signers = [&safe_authority, client.payer()];
+    client
+				.inner
+        .set_authority_with_signers(&signers, &accounts, new_authority)
+        .unwrap();
+
+    // Then.
+    //
+    // The safe account should be updated.
+    {
+        let safe_acc = client.safe(&safe_acc).unwrap();
+        assert_eq!(safe_acc.authority, new_authority);
+    }
+}
