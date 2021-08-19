f82b027bdec956775b2fb509f390e389b2add7e1
dd <dafydd.durairaj@gmail.com>
2021-08-19 12:40:25 -0400

    added back in devnet compile flag

--- dex/src/instruction.rs
+++ dex/src/instruction.rs
@@ -21,11 +21,17 @@ use proptest_derive::Arbitrary;
 
 pub mod srm_token {
     use solana_program::declare_id;
+    #[cfg(feature = "devnet")]
+    declare_id!("AvtB6w9xboLwA145E221vhof5TddhqsChYcx7Fy3xVMH");
+    #[cfg(not(feature = "devnet"))]
     declare_id!("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt");
 }
 
 pub mod msrm_token {
     use solana_program::declare_id;
+    #[cfg(feature = "devnet")]
+    declare_id!("8DJBo4bF4mHNxobjdax3BL9RMh5o71Jf8UiKsf5C5eVH");
+    #[cfg(not(feature = "devnet"))]
     declare_id!("MSRMcoVyrFxnSgo5uXwone5SKcGhT1KEJMFEkMEWf9L");
 }
 
