b6bdebeb66ae5a32893d176fe6a2ed3d7ab34f0e
Kelvin Lau <klau@Kelvins-MacBook-Pro.local>
2023-04-13 15:23:29 +0800

    temp fix

--- dex/src/state.rs
+++ dex/src/state.rs
@@ -2945,33 +2945,39 @@ fn action_out_event(
     client_order_id: Option<NonZeroU64>,
 ) -> DexResult {
     check_assert!(owner_slot < 128)?;
-    check_assert_eq!(&open_orders.slot_side(owner_slot), &Some(side))?;
-    check_assert_eq!(open_orders.orders[owner_slot as usize], order_id)?;
+    // check_assert_eq!(&open_orders.slot_side(owner_slot), &Some(side))?;
+    // check_assert_eq!(open_orders.orders[owner_slot as usize], order_id)?;
 
     let fully_out = native_qty_still_locked == 0;
-
-    match side {
-        Side::Bid => {
-            if release_funds {
-                open_orders.native_pc_free += native_qty_unlocked;
+    if open_orders.orders[owner_slot as usize] == order_id
+        && open_orders.slot_side(owner_slot) == Some(side)
+    {
+        match side {
+            Side::Bid => {
+                if release_funds {
+                    open_orders.native_pc_free += native_qty_unlocked;
+                }
+                check_assert!(open_orders.native_pc_free <= open_orders.native_pc_total)?;
             }
-            check_assert!(open_orders.native_pc_free <= open_orders.native_pc_total)?;
-        }
-        Side::Ask => {
-            if release_funds {
-                open_orders.native_coin_free += native_qty_unlocked;
+            Side::Ask => {
+                if release_funds {
+                    open_orders.native_coin_free += native_qty_unlocked;
+                }
+                check_assert!(open_orders.native_coin_free <= open_orders.native_coin_total)?;
             }
-            check_assert!(open_orders.native_coin_free <= open_orders.native_coin_total)?;
+        };
+        if let Some(client_id) = client_order_id {
+            debug_assert_eq!(
+                client_id.get(),
+                identity(open_orders.client_order_ids[owner_slot as usize])
+            );
         }
-    };
-    if let Some(client_id) = client_order_id {
-        debug_assert_eq!(
-            client_id.get(),
-            identity(open_orders.client_order_ids[owner_slot as usize])
-        );
-    }
-    if fully_out {
-        open_orders.remove_order(owner_slot)?;
+        if fully_out {
+            open_orders.remove_order(owner_slot)?;
+        }
+    } else {
+        check_assert!(!release_funds)?;
+        check_assert!(!fully_out)?;
     }
 
     Ok(())
